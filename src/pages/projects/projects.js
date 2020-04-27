3db73e0d864b9fae11ecb3b5f547055c07e42b10
Valiana <valiana.josic@gmail.com>
2020-04-27 19:14:12 -0400

    Card CSS

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -2,31 +2,45 @@ import React from 'react';
 import './projects.css';
 import { Card, Button } from 'react-bootstrap';
 import Thais from '../../assets/images/thais.png'
+import Arcadia from '../../assets/images/arcadia.png'
 
 
 function Projects() {
   return (
     <div className='card-wrapper' >
-    <Card className= 'card'style={{ width: '18rem' }}>
-      <Card.Img style={{ width: '18rem'}} variant="top" src={Thais} />
-      <Card.Body>
-        <Card.Title className='card-title'>Realtor Website</Card.Title>
-        <Card.Text  className='card-text'>
-          My first project ! Designed and developed Realtor website for client.
+      <Card className='card' style={{ width: '18rem' }}>
+        <Card.Img style={{ width: '288px', height: '136.03px' }} variant="top" src={Arcadia} />
+        <Card.Body>
+          <Card.Title className='card-title'>Arcadia</Card.Title>
+          <Card.Text className='card-text'>
+            Wyncode Academy Final Project for Graduation day. Worked together with a UX/UI and developer team.
       </Card.Text>
-        <Button className= 'card-btn' variant="primary"><a className= 'card-btn-font'href='https://thais.herokuapp.com/'  target="_blank" rel="noopener  noreferrer" >Check it out!</a></Button>
-      </Card.Body>
-    </Card>
-    <Card className= 'card'style={{ width: '18rem' }}>
-      <Card.Img style={{ width: '18rem' }} variant="top" src={Thais} />
-      <Card.Body>
-        <Card.Title className='card-title'>Realtor Website</Card.Title>
-        <Card.Text  className='card-text'>
-          My first project ! Designed and developed Realtor website for client.
+          <Button className='card-btn' variant="primary"><a className='card-btn-font' href='https://arcadiaivf.herokuapp.com/' target="_blank" rel="noopener  noreferrer" >Arcadia.com</a></Button>
+        </Card.Body>
+      </Card>
+
+      <Card className='card' style={{ width: '18rem' }}>
+        <Card.Img style={{ width: '18rem' }} variant="top" src={Thais} />
+        <Card.Body>
+          <Card.Title className='card-title'>Realtor Website</Card.Title>
+          <Card.Text className='card-text'>
+            My first project as a graduated Full-Stack Web Developer ! Designed and developed Realtor website for client.
       </Card.Text>
-        <Button className= 'card-btn' variant="primary"><a className= 'card-btn-font'href='https://thais.herokuapp.com/'  target="_blank" rel="noopener  noreferrer" >Check it out!</a></Button>
-      </Card.Body>
-    </Card>
+          <Button className='card-btn' variant="primary"><a className='card-btn-font' href='https://thais.herokuapp.com/' target="_blank" rel="noopener  noreferrer" >Thaisreyes.com</a></Button>
+        </Card.Body>
+      </Card>
+
+      <Card className='card' style={{ width: '18rem' }}>
+        <Card.Img style={{ width: '18rem' }} variant="top" src={Thais} />
+        <Card.Body>
+          <Card.Title className='card-title'>Realtor Website</Card.Title>
+          <Card.Text className='card-text'>
+            My first project as a graduated Full-Stack Web Developer ! Designed and developed Realtor website for client.
+      </Card.Text>
+          <Button className='card-btn' variant="primary"><a className='card-btn-font' href='https://thais.herokuapp.com/' target="_blank" rel="noopener  noreferrer" >Check it out!</a></Button>
+        </Card.Body>
+      </Card>
+      
     </div>
   );
 }
