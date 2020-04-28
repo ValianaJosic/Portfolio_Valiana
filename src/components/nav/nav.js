fbb6cba43efed97a5a8ec22301730604d3e9af65
Valiana <valiana.josic@gmail.com>
2020-04-28 12:33:55 -0400

    Added git icon to footer and nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -53,6 +53,9 @@ class Nav extends Component {
                 <li className="nav-link"><a href="https://www.linkedin.com/in/valiana-josic/" target="_blank" rel="noopener  noreferrer" className="nav-name">
                     <i className="fa fa-linkedin"></i>
                 </a></li>
+                <li className="nav-link"><a href="https://github.com/ValianaJosic" target="_blank" rel="noopener  noreferrer" className="nav-name">
+                <i class="fa fa-github" aria-hidden="true"></i>
+                </a></li>
             </ul>
         )
     }
