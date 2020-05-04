3043125a952e2c3ac445fc00af9035ff67d36c6c
Valiana <valiana.josic@gmail.com>
2020-05-04 16:43:24 -0400

    NAV CSS

--- src/pages/home/about-me/about-me.js
+++ src/pages/home/about-me/about-me.js
@@ -16,7 +16,7 @@ function About() {
             I worked in hospitality for 9 years and entered a coding bootcamp ( Wyncode !) wanting to create a better future for my daughter and I.
             <br></br>
             <br></br>
-            Hispanic women are considered  minorities in the tech field so I wanted to break these norms and create more possibilities for my daughters future.  Now as a Web Developer I've already shown my daughter you can do anything with persitance and hard work.
+            Hispanic women are considered  minorities in the tech field so I wanted to break these norms and create more possibilities for my daughters future.  Now as a Web Developer I've already shown my daughter you can do anything with persistence and hard work.
             <br></br>
             <br></br>
             I strongly believe in using tech for good by implementing solutions to improve our society and environment in a positive direction. 
